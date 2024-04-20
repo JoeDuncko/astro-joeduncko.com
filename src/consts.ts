eabb63a9f0a5cea31f99db7839c0e571a6bd05bf
Joe Duncko <JoeDuncko@users.noreply.github.com>
2024-04-20 19:37:20 -0400

    Home page sorta working

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const MY_NAME = "Joe Duncko";
+
+export const SITE_DESCRIPTION =
+    "Frontend engineer and community organizer based in Youngstown, Ohio";
+
+export const CATCH_PHASE = "Content Catalyst";
+
+export const SITE_TITLE = `${MY_NAME} | ${CATCH_PHASE}`;
