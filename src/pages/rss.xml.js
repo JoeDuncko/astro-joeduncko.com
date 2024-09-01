df5da7fc073165acbd11e7cf630a30997c4e7987
Joe Duncko <JoeDuncko@users.noreply.github.com>
2024-09-01 11:30:33 -0400

    Fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
     const posts = await getCollection("blog");
@@ -8,9 +8,11 @@ export async function GET(context) {
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-            link: `/blog/${post.slug}/`,
-        })),
+        items: posts
+            .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+            .map((post) => ({
+                ...post.data,
+                link: `/blog/${post.slug}/`,
+            })),
     });
 }
